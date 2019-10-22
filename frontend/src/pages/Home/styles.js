322957efe79f3b226050dd7f7f0eb3d0eaf562a9
v-raina <alina.aldhytha@gmail.com>
2019-10-22 18:20:24 +1100

    Added home page and navigation bar UI

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,16 +1,43 @@
 import { createStyles } from "@material-ui/core/styles";
 
 export const styles = createStyles(theme => ({
-  home: {
-    paddingTop: "20vh",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center"
-  },
   title: {
     textAlign: "center",
-    paddingTop: "20px",
-    paddingBottom: "20px"
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  bg: {
+    maxWidth: "100%",
+    height: "auto"
+  },
+  bg_layer: {
+    zIndex: 1110,
+    position: "fixed",
+    top: 64,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "black",
+    opacity: 0.7
+  },
+  center_container: {
+    zIndex: 1111,
+    position: "absolute",
+    width: "100%",
+    top: "35%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  description: {
+    color: "white",
+    padding: 5
+  },
+  start_button: {
+    marginTop: 40
+  },
+  start_text: {
+    color: "white",
+    fontStyle: "inherit"
   }
 }));
