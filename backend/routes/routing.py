27bc9801446e5dd76de9b905712e2c3c9b6d7abd
Kevin Liyanto <k.liyanto@unsw.edu.au>
2019-11-01 05:06:57 +1100

    Renamed model, added routing
    - Renamed model for clarity
    - Added more categories for the frontend->FSAPI flow
    - Added template for routing including its modelnamed model, added routing

--- backend/routes/routing.py
+++ backend/routes/routing.py
@@ -1,4 +1,26 @@
+import requests
+import json
+import config
+from datetime import date, timedelta
+
+
+from concurrent.futures import ThreadPoolExecutor
+
+from app import api
+from flask_restplus import Resource, abort, reqparse, fields
+from flask import request, jsonify
+from requests_futures.sessions import FuturesSession
+
 from util.models import *
 
 
 routing = api.namespace('routing', description='Routing of places')
+
+@routing.route('/itinerary', strict_slashes=False)
+class ItineraryAlgorithm(Resource):
+    @routing.response(200, 'Success')
+    @routing.expect(route_array)
+    def post(self):
+        received_item = request.json
+        pass
+
