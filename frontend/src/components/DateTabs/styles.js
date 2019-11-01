3d0551deffaa53a6779f6b4acbf3a5292e7dee5a
Sebastian Arthur Chua <sebastian.a.chua@gmail.com>
2019-11-01 12:29:36 +1100

    Finished E2US2

--- frontend/src/components/DateTabs/styles.js
+++ frontend/src/components/DateTabs/styles.js
@@ -2,20 +2,18 @@ import { createStyles } from "@material-ui/core/styles";
 
 export const styles = createStyles(theme => ({
     transparentTabs: {
-        position: "-webkit-sticky",
         position: "sticky",
-        top: 0,
+        top: 64,
         zIndex: 999,
         transition: "background-color 0.3s ease 0s",
         borderBottom: "1px solid black",
         backgroundColor: "transparent"
     },
     colouredTabs: {
-        position: "-webkit-sticky",
         position: "sticky",
-        top: 0,
+        top: 64,
         zIndex: 999,
         transition: "background-color 0.3s ease 0s",
-        backgroundColor: "#FF8D77"
+        backgroundColor: theme.palette.tertiary.main
     }
 }))
