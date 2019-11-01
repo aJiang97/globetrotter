be686ad614cd75af2eea3c8b62bcbfa68fdd968f
Kevin Liyanto <k.liyanto@unsw.edu.au>
2019-11-02 06:30:11 +1100

    Refactor and new endpoint
    - Refactored the model interpretation to ensure that all follows the same model (under
      model_interpret)
    - Added endpoint on suggest which combines FS and Google
    - Bug found: ['venue']['location'] of FS API venue (either short detail from explore endpoint or
      venue endpoint) might not contain the required key for Google WS PlaceID search. Should be simple
      fix (leaving out the None items on .get(key) call)

---
+++ backend/util/model_interpret.py
@@ -0,0 +1,72 @@
+# Move the details proc to this part, to uniform all endpoints
+
+class ModelProc:
+    def f_location(self, jsonobject):
+        locs = {
+            "romance": False,
+            "nature": False,
+            "wildlife": False,
+            "shopping": False,
+            "historical": False,
+            "cultural": False,
+            "family": False,
+            "beaches": False,
+            "food": False
+        }
+
+        pics = []
+
+        # Add best photo
+        bp = jsonobject['response']['venue']['bestPhoto']
+        pics.append(bp['prefix'] + str(bp['width']) +
+                    'x' + str(bp['height']) + bp['suffix'])
+
+        coords = {
+            "latitude": jsonobject['response']['venue']['location']['lat'],
+            "longitude": jsonobject['response']['venue']['location']['lng']
+        }
+
+        return {
+            "venue_name": jsonobject['response']['venue']['name'],
+            "location_types": locs,
+            "coordinate": coords,
+            "pictures": pics,
+            "location_id": jsonobject['response']['venue']['id'],
+            "url": jsonobject['response']['venue'].get('url'),
+            "description": jsonobject['response']['venue'].get('description')
+        }
+
+    def g_location(self, jsonobject):
+        #get location rating
+        if jsonobject['result'].get('rating') != None:
+            rating = jsonobject['result']['rating']
+        else:
+            rating = None
+
+        #get user reviews
+        user_reviews = []
+        if jsonobject['result'].get('reviews') != None: 
+            for i in range(len(jsonobject['result']['reviews'])-1):
+                user_reviews.append({
+                    "name": jsonobject['result']['reviews'][i]['author_name'],
+                    "review": jsonobject['result']['reviews'][i]['text'],
+                    "time": jsonobject['result']['reviews'][i]['relative_time_description']
+                })
+
+        # links to googlemaps of location
+        url = jsonobject['result']['url']
+
+        #not sure this is needed in the google endpoint as it already exists in fs endpoint
+        coords = {
+            'latitude': 0,# jsonobject['result']['geometry']['location']['lat'],
+            'longitude':0 # jsonobject['result']['geometry']['location']['lng']
+        }
+
+        return {
+            'location_name': name,
+            'reviews': user_reviews,
+            'rating': rating,
+            'maps_url': url,
+            'place_id': place_id,
+            'coordinate': coords
+        }
