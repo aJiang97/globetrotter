a1c8cd776f41b29c682590636d4b48573390812f
Natasha Jenny <natashajenny06@gmail.com>
2019-11-05 22:04:47 +1100

    tidied up map, still need to figure out infowindow

--- frontend/src/components/MapContainer/MapContainer.jsx
+++ frontend/src/components/MapContainer/MapContainer.jsx
@@ -9,62 +9,87 @@ const style = {
 
 
 export class MapContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {}
-    }
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    this.onClose = this.onClose.bind(this);
-  }
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
 
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true
-    })
+      showingInfoWindow: true,
+    });
   
-  onClose = props => {
+
+  onMapClicked = (props) => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
         activeMarker: null
       })
     }
-  }
+  };
 
-  renderMarkers() {
-    return this.props.locations.map((loc,key) => {
-      return (<Marker onClick={this.onMarkerClick} position={{
-        lat: loc.lat,
-        lng: loc.lng
-      }} name={loc.title} />);
-    })
-  }
+  // renderMarkers() {
+  //   return this.props.locations.map((loc,key) => {
+  //       <Marker
+  //         key={key}
+  //         onClick={this.onMarkerClick}
+  //         position={{
+  //           lat: loc.lat,
+  //           lng: loc.lng
+  //         }}
+  //         name={loc.title} 
+  //       />
+  //       <InfoWindow
+  //         marker={this.state.activeMarker}
+  //         visible={this.state.showingInfoWindow}
+  //       >
+  //         <div>
+  //           <h1>HELLO</h1>
+  //         </div>
+  //       </InfoWindow>
+  //     );
+  //   })
+  // }
 
   render() {
-    const { classes } = this.props;
     return (
       <Map
         google={this.props.google}
+        onClick = {this.onMapClicked}
         style={style}
         zoom={12}
-        disableDefaultUI="true"
+        disableDefaultUI= {true}
         // This is Sydney centre coordinates
         initialCenter={{
           lat: -33.8708,
           lng: 151.2073
         }}
       >
-        {this.renderMarkers()}
-        {/* <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow} onClose={this.onClose}>
-          <div>
-            {this.state.selectedPlace.name}
-          </div>
-        </InfoWindow>); */}
+      {this.props.locations.map((loc,key) => (
+          <Marker
+            key={key}
+            onClick={this.onMarkerClick}
+            position={{
+              lat: loc.lat,
+              lng: loc.lng
+            }}
+            name={loc.title} 
+          >
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+            >
+              <div>
+                <h1>HELLO</h1>
+              </div>
+            </InfoWindow>
+          </Marker>
+      ))}
       </Map>
     )
   }
