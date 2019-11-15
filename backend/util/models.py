3e0e0c13d0335868df673dca46e1f1c7ed0e744f
Kevin Liyanto <k.liyanto@unsw.edu.au>
2019-11-15 15:01:12 +1100

    Compressed trip payload to a BLOB

--- backend/util/models.py
+++ backend/util/models.py
@@ -203,14 +203,19 @@ MODEL_trip_detail = api.model('detail', {
     "description": fields.String(description='Description of the trip. Can be empty string',required=True),
     "location": fields.String(description='Location of the trip', required=True),
     "tripstart": fields.DateTime(dt_format='iso8601', description='Start time of the trip, format is ISO8601. Read https://en.wikipedia.org/wiki/ISO_8601', required=True),
-    "tripend": fields.DateTime(dt_format='iso8601', description='End time of the trip', required=True)
+    "tripend": fields.DateTime(dt_format='iso8601', description='End time of the trip', required=True),
+    "modifiedddate": fields.DateTime(dt_format='iso8601', description='', required=True)
 })
 
 MODEL_trip_payload = api.model('payload', {
     "details": fields.Nested(MODEL_trip_detail, required=True),
+<<<<<<< Updated upstream
     "matrix": fields.Raw(description='JSONObject of the matrix', required=True),
     "matrix_places": fields.Raw(description='JSONObject of the index of the matrix', required=True),
     "ordered_places": fields.Raw(description='JSONObject of the ordered places', required=True),
     "calendar": fields.String(description='Calendar in byte array or string format. Backend will store it as-is', required=True)
+=======
+    "blob": fields.Raw(description='Your blob', required=True)
+>>>>>>> Stashed changes
 })
 
