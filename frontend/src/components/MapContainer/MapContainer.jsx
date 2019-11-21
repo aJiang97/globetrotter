65f124abd5501c1bf78e9cf628fc5f991dccc30d
Natasha Jenny <natashajenny06@gmail.com>
2019-11-21 19:06:30 +1100

    Infowindow, path; todo

--- frontend/src/components/MapContainer/MapContainer.jsx
+++ frontend/src/components/MapContainer/MapContainer.jsx
@@ -1,4 +1,5 @@
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+/* global google */
+import { Map, GoogleApiWrapper, InfoWindow, DirectionsRenderer, Marker } from 'google-maps-react';
 import * as React from "react";
 
 const style = {
@@ -12,17 +13,28 @@ export class MapContainer extends React.Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
-    selectedPlace: {}
+    selectedPlace: {},
+    directions: {}
   };
 
+  createCoordinatesList() {
+    var names = ['lat', 'lng'], coordinates = [];
+    this.props.locations.map(loc => {
+      var array = {};
+      array[names[0]] = parseFloat(loc.foursquare.coordinate.latitude);
+      array[names[1]] = parseFloat(loc.foursquare.coordinate.longitude);
+      coordinates.push(array);
+    })
+    return coordinates;
+  }
 
-  onMarkerClick = (props, marker, e) =>
+  onMarkerClick = (props, marker, e) => {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
       showingInfoWindow: true,
     });
-  
+  }
 
   onMapClicked = (props) => {
     if (this.state.showingInfoWindow) {
@@ -33,41 +45,82 @@ export class MapContainer extends React.Component {
     }
   };
 
+  // getDirections() {
+  //   const coordinates = this.createCoordinatesList();
+  //   var waypoints = [];
+  //   var start = null;
+  //   var end = null;
+  //   for (var i = 0; i < coordinates.length; i++) {
+  //     var array = {};
+  //     array['location'] = new google.maps.LatLng(coordinates[i].lat, coordinates[i].lng);
+  //     if (i === 0) {
+  //       start = array['location'];
+  //     } else if (i === coordinates.length - 1) {
+  //       end = array['location'];
+  //     }
+  //     waypoints.push(array);
+  //   }
+
+  //   const DirectionsService = new google.maps.DirectionsService();
+  //   DirectionsService.route(
+  //     {
+  //       origin: start,
+  //       destination: end,
+  //       travelMode: google.maps.TravelMode.DRIVING,
+  //       waypoints: waypoints
+  //     },
+  //     (result, status) => {
+  //       console.log(status);
+  //       if (status === google.maps.DirectionsStatus.OK) {
+  //         this.setState({
+  //           directions: result
+  //         });
+  //       } else {
+  //         console.error('error fetching directions ${result}');
+  //       }
+  //     }
+  //   );
+  // }
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        onClick = {this.onMapClicked}
-        style={style}
-        zoom={12}
-        disableDefaultUI= {true}
-        // This is Sydney centre coordinates
-        initialCenter={{
-          lat: this.props.locations[0].foursquare.coordinate.latitude,
-          lng: this.props.locations[0].foursquare.coordinate.longitude
-        }}
-      >
-      {this.props.locations.map((loc,key) => (
-          <Marker
-            key={key}
-            onClick={this.onMarkerClick}
-            position={{
-              lat: loc.foursquare.coordinate.latitude,
-              lng: loc.foursquare.coordinate.longitude
-            }}
-            name={loc.foursquare.venue_name} 
+      <div>
+        <Map
+          google={this.props.google}
+          onClick = {this.onMapClicked}
+          style={style}
+          zoom={12}
+          disableDefaultUI= {true}
+          // This is Sydney centre coordinates
+          initialCenter={{
+            lat: this.props.locations[0].foursquare.coordinate.latitude,
+            lng: this.props.locations[0].foursquare.coordinate.longitude
+          }}
+        >
+          {this.props.locations.map((loc,key) => (
+              <Marker
+                key={key}
+                onClick={this.onMarkerClick}
+                position={{
+                  lat: loc.foursquare.coordinate.latitude,
+                  lng: loc.foursquare.coordinate.longitude
+                }}
+                name={loc.foursquare.venue_name} 
+              />
+          ))}
+          {this.state.activeMarker && <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
           >
-            {/* <InfoWindow
-              marker={this.state.activeMarker}
-              visible={this.state.showingInfoWindow}
-            >
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-            </InfoWindow> */}
-          </Marker>
-      ))}
-      </Map>
+            <div>
+              <h2>{this.state.selectedPlace.name}</h2>
+            </div>
+          </InfoWindow>}
+          {/* {this.getDirections()}
+          {this.props.directions && <DirectionsRenderer directions = {this.props.directions} />} */}
+        {/* {console.log(this.createCoordinatesList())} */}
+        </Map>
+      </div>
     )
   }
 }
@@ -75,3 +128,4 @@ export class MapContainer extends React.Component {
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyB376cyeRoqfXHQXE-Zhl45CP8sPSK4MV0'
 })(MapContainer);
+
