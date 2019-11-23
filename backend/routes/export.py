95779ac58f9d1ee75eecc2a6cd5c29c576496e0f
Alex Jiang <alex.jiang@student.unsw.edu.au>
2019-11-23 23:31:38 +1100

    update to account for timezone

--- backend/routes/export.py
+++ backend/routes/export.py
@@ -12,26 +12,54 @@ class exportTrip(Resource):
     @export.param('itinerary','The trip the user wants to export to ics format')
     @export.expect(MODEL_calendar)
     # 'itinerary':[{
-    #               'name':'"{"name of event"}",
-    #               'start':"start time of event"}",
+    #               'name':"name of event",
+    #               'start':"start time of event",
     #               'duration':duration of even in minutes
     #               },
     #               {next event details...}
     #              ]
+
+# example...
+#     {
+#   "itinerary": [
+#     {
+#       "name": "opera house",
+#       "start": "2019-11-25 09:00:00",
+#       "duration": 60
+#     },
+#     {
+#       "name": "harbour bridge",
+#       "start": "2019-11-25 11:00:00",
+#       "duration": 60
+#     },
+#     {
+#       "name": "bondi beach",
+#       "start": "2019-11-25 04:00:00",
+#       "duration": 60
+#     }
+#   ]
+# }
 
     def post(self):
         data = request.get_json()
         if data is None:
             abort(400,'Trip Data not received')
         itinerary = data.get('itinerary')
-        #start_dates = data.get('startdate')
 
+        # start_date = data.get('startdate')
+        # start = datetime.strptime(start_date,'%y-%m-%d %H:%M:%S')
+        # # account for gmt, sydney+11
+        # start = start - timedelta(hours=-11)
+        # # assume starting day at 9am
+        # start = start + timedelta(hours=9)
 
         c = Calendar()
         for event in itinerary:
             e = Event()
             e.name = event['name']
-            e.begin = event['start']
+            e.begin = datetime.strptime(event['start'],'%Y-%m-%d %H:%M:%S')+timedelta(hours=-11) #account for tz
             e.duration = timedelta(minutes=event['duration'])
             c.events.add(e)
+        # with open('my.ics', 'w') as f:
+        #     f.write(str(c))
         return str(c)
